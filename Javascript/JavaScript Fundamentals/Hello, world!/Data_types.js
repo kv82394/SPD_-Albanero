095495d001fc0d896b83ccb2c288575ebf28d6ee
kv82394 <kv82394@gmail.com>
2021-03-09 12:06:58 +0530

    Add files via upload

---
+++ Javascript/JavaScript Fundamentals/Hello, world!/Data_types.js
@@ -0,0 +1,17 @@
+console.log("Hello world");
+let my_name = "vivek";
+console.log(my_name);
+console.log(my_name.length);
+
+console.log(`Hello ${my_name} its works`);
+let x = 1;
+console.log(`value of 3 + ${x} = ${3+x}`);
+let bool_value = 56<7
+console.log(bool_value)
+console.log(typeof bool_value)
+console.log(typeof(bool_value))//also works
+
+
+
+console.log( `hello ${"my_name"}` );
+console.log( `hello ${my_name}` );
