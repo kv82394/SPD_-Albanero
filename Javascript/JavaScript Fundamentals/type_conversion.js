7067e602a57cf7834e40ac32f4fd9ceac5c63495
kv82394 <kv82394@gmail.com>
2021-03-09 12:37:43 +0530

    Add files via upload

---
+++ Javascript/JavaScript Fundamentals/type_conversion.js
@@ -0,0 +1,24 @@
+let val = true
+console.log(val)
+console.log(typeof(val))
+val = String(val)// capital S when writing string
+console.log(typeof(val))
+val = 1
+console.log(typeof(val))
+val = String(val)// capital S when writing string
+console.log(typeof(val))
+
+
+// Numeric Conversion
+val = "123"
+val = Number(val)
+console.log(typeof val)
+//implicit type conversion
+console.log("5"/"5")
+console.log("5"/5)
+console.log(Boolean('0'))
+// The three most widely used type conversions are to string, to number, and to boolean.
+
+// String Conversion – Occurs when we output something. Can be performed with String(value). The conversion to string is usually obvious for primitive values.
+
+// Numeric Conversion – Occurs in math operations. Can be performed with Number(value).
