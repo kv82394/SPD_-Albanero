95052c005aa723b0e68c182b53c5d251ddd0c628
kv82394 <kv82394@gmail.com>
2021-03-09 11:40:32 +0530

    Add files via upload

---
+++ Javascript/JavaScript Fundamentals/Hello, world!/variables.js
@@ -0,0 +1,21 @@
+/*
+Tasks
+Working with variables
+Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output “John”).
+*/
+let admin, x; 
+
+x = "John";
+
+admin = x;
+
+console.log(x);
+
+/*tASK 2
+Giving the right name
+Create a variable with the name of our planet. How would you name such a variable?
+Create a variable to store the name of a current visitor to a website. How would you name that variable? */
+let planet = "earth";
