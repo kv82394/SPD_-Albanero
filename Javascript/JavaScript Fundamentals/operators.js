6077d2741c1f501e90b86345eedaa68ef70ddb9d
kv82394 <kv82394@gmail.com>
2021-03-09 13:20:17 +0530

    Add files via upload

---
+++ Javascript/JavaScript Fundamentals/operators.js
@@ -0,0 +1,34 @@
+val = -1
+val = -val
+console.log(val)
+let a = 10 ,b = 5;
+console.log(`Addition : ${a+b}`)
+console.log(`Substraction : ${a-b}`)
+console.log(`Multiply : ${a*b}`)
+console.log(`Division : ${a/b}`)// floor division not possible
+console.log(`Division : ${46/b}`)
+console.log(`Mod : ${a%b}`)
+console.log(`Exponential : ${a**b}`)
+
+
+
+//string concatinate 
+let str1 = "kumar",str2 = "vivek"
+console.log(str1 +" "+ str2)
+console.log("1" + 2)
+console.log(1 + "2")
+console.log(1 + 1+ "2")
+console.log("1"+ 1+ 2)//rem 
+let x = 1;
+let y = 2;
+let z = x+ (y=x+2)
+console.log(x,y,z)
+
+x = y = z = 1
+console.log(x,y,z)
+
+
+z = (1 + 2, 3 + 4)
+console.log(z)
+z = (3,7)
+console.log(z)
