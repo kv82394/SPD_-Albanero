182cde4b8e46944f15abe199dec74b346b02ee59
kv82394 <kv82394@gmail.com>
2021-03-09 18:47:52 +0530

    Update function.js

--- Javascript/JavaScript Fundamentals/function.js
+++ Javascript/JavaScript Fundamentals/function.js
@@ -31,3 +31,39 @@ function calPower(x,n){
     return sum;
 }
 console.log(calPower(2,5));
+let sayHi = function() {
+    console.log( "Hello" );
+  };//this function will work also if we remove the semicolon
+sayHi()
+let sayHello = sayHi// copying the function 
+sayHello()
+
+// callback
+function calculator(sum , addition ,multiply,choice,val1,val2){
+    switch (choice){
+        case 1:
+            sum(val1,val2)
+            break;
+        case 2 :
+            multiply(val1,val2)
+            break;
+        case 3:
+            multiply(val1,val2)
+            break;
+        default:
+            console.log("invalid input")
+
+    }
+
+}
+function sum(a,b){
+    console.log(a+b)
+}
+function multiply(a,b){
+    console.log(a*b)
+}
+function addition(a,b){
+    console.log(a+b)
+}
+calculator(sum,addition,multiply,3,10,20)
+
