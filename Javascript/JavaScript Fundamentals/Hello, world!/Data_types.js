a6b553e28a91332cd4a8ea9021898092e8b43596
kv82394 <kv82394@gmail.com>
2021-03-09 12:07:53 +0530

    Update Data_types.js

--- Javascript/JavaScript Fundamentals/Hello, world!/Data_types.js
+++ Javascript/JavaScript Fundamentals/Hello, world!/Data_types.js
@@ -15,3 +15,13 @@ console.log(typeof(bool_value))//also works
 
 console.log( `hello ${"my_name"}` );
 console.log( `hello ${my_name}` );
+There are 8 basic data types in JavaScript.
+
+// number for numbers of any kind: integer or floating-point, integers are limited by ±(253-1).
+// bigint is for integer numbers of arbitrary length.
+// string for strings. A string may have zero or more characters, there’s no separate single-character type.
+// boolean for true/false.
+// null for unknown values – a standalone type that has a single value null.
+// undefined for unassigned values – a standalone type that has a single value undefined.
+// object for more complex data structures.
+// symbol for unique identifiers.
