78e462fadec84dfbce662b875238e2f8b8133564
kv82394 <kv82394@gmail.com>
2021-03-09 12:20:40 +0530

    Add files via upload

---
+++ Javascript/JavaScript Fundamentals/alert,_prompt,_confirm.js
@@ -0,0 +1,36 @@
+/*these all function works only for browser
+---------------->alert<------------------------------
+alert("Hello");//pops up Hello 
+
+
+
+
+
+
+
+
+---------------->prompt<------------------------------
+The function prompt accepts two arguments
+result = prompt(title, [default]);
+example 
+let age = prompt('How old are you?', 100);// this ask you to give a input if u didn't provide it will consider 100 as default
+
+alert(`You are ${age} years old!`); //
+
+
+
+
+
+
+
+
+
+
+---------------->confirm<------------------------------
+result = confirm(question);
+question is a string which is visible an if we click on yes then true is stored in result otherwise false is stored in result
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss ); // true if OK is pressed
+
+*/
