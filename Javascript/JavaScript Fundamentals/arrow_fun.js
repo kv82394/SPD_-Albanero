30ce2a0e2b3af7f94e639214b991170ff357ddc0
kv82394 <kv82394@gmail.com>
2021-03-09 18:56:34 +0530

    Add files via upload

---
+++ Javascript/JavaScript Fundamentals/arrow_fun.js
@@ -0,0 +1,24 @@
+//syntax
+//let func = (arg1, arg2, ..., argN) => expression
+let sum = (a, b) => a + b;//no need to return any value
+console.log(sum(56,1))
+//multiline 
+ let sum1 = (a,b,c)=>{
+     let x = a+b
+     x = x+c
+     return x;
+ }
+ console.log(sum1(2,3,6));
+ //task
+//  function ask(question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+//   }
+  
+//   ask(
+//     "Do you agree?",
+//     function() { alert("You agreed."); },
+//     function() { alert("You canceled the execution."); }
+let ask = (question,yes,no)=>{
+    ///statment
+}
