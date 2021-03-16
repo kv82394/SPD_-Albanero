883213231c8c856aeb8d00e3abdc118482e0872a
kv82394 <kv82394@gmail.com>
2021-03-16 18:49:58 +0530

    Create property_getters_and_setters.js

---
+++ Javascript/Object properties configuration/property_getters_and_setters.js
@@ -0,0 +1,28 @@
+let user = {
+    name: "John",
+    surname: "Smith",
+  
+    get fullName() {
+      return `${this.name} ${this.surname}`;
+    }
+  };
+  
+  console.log(user.fullName); // John Smith
+   user = {
+    name: "John",
+    surname: "Smith"
+  };
+  
+  Object.defineProperty(user, 'fullName', {
+    get() {
+      return `${this.name} ${this.surname}`;
+    },
+  
+    set(value) {
+      [this.name, this.surname] = value.split(" ");
+    }
+  });
+  
+  alert(user.fullName); // John Smith
+  
+  for(let key in user) alert(key); // name, surname
