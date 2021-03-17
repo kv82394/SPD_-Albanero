9bbfbc24146095d269984a9703590b4662a7f12a
kv82394 <kv82394@gmail.com>
2021-03-17 09:02:44 +0530

    Update function.prototype.js

--- Javascript/Prototypes inheritance/function.prototype.js
+++ Javascript/Prototypes inheritance/function.prototype.js
@@ -21,3 +21,32 @@ let rabbit = new Rabbit();
 Rabbit.prototype.eats = false;
 
 console.log( rabbit.eats );
+function Rabbit() {}
+// by default:
+// Rabbit.prototype = { constructor: Rabbit }
+
+console.log( Rabbit.prototype.constructor == Rabbit );
+function Rabbit(name) {
+  this.name = name;
+  alert(name);
+}
+
+let rabbit = new Rabbit("White Rabbit");
+
+let rabbit2 = new rabbit.constructor("Black Rabbit");
+//task 1 
+
+//Changing "prototype"
+function Rabbit() {}
+Rabbit.prototype = {
+  eats: true
+};
+
+let rabbit = new Rabbit();
+
+Rabbit.prototype = {};//true
+Rabbit.prototype.eats = false;//false
+delete rabbit.eats;//true
+delete Rabbit.prototype.eats;//undefined
+
+alert( rabbit.eats ); // ?
